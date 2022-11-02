850912404d9729b40832e2b8b1f48a2da19e9e7f
Fathur Muhammad Fadlan Anshari <anshariF17@gmail.com>
2022-11-02 16:09:44 +0700

    adding smooth scrool info section

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -11,7 +11,43 @@ export const homeObjOne = {
   imgStart: false,
   img: require("../../images/svg-1.svg").default,
   alt: "Car",
-  darl: true,
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headLine: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/svg-2.svg").default,
+  alt: "Piggybank",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our Team",
+  headLine: "Creating an account is extremly easy",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/svg-3.svg").default,
+  alt: "Car",
+  dark: true,
   primary: true,
   darkText: false,
 };
